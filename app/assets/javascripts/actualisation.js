b878b6cf4b05fb555acfb9954c24aa302a93af38
Quentin33 <quentin.pauzat@ingesup.com>
2012-02-16 00:18:47 +0100

    Amélioration de l'interface avec JQuery et finalisation de Socket.IO

--- app/assets/javascripts/actualisation.js
+++ app/assets/javascripts/actualisation.js
@@ -25,7 +25,7 @@ $(document).ready(function()
         //On affiche un message en fonction du nombre de nouveaux tweets
         if(numberNewTweets.length == 1)
         {
-            $('#nouveaux_tweets').html("<div class='alert-message info' style='text-align:center;'><a href='/' style='color:#FFFFFF;'>Il y a de nouveaux messages</a></div>");    
+            $('#nouveaux_tweets').html("<div class='alert-message info' style='text-align:center;'><a href='/' style='color:#FFFFFF;'>Il y a de nouveaux twits</a></div>");    
         }
 
     }
@@ -45,6 +45,7 @@ $(document).ready(function()
     $('#btnEnvoiTweet').click(function() 
     {
         send_tweet($('#message').val(), $('meta[name=csrf-token]').attr('content'));
+        $('#nouveaux_tweets').html("<p><b><span class='label notice'>Créé il y a moins d'une minute</span> &nbsp; <a href='/users/tweet/" + $("#DivCurrentUser").html() + "'><span class='label success'>par " + $("#DivCurrentUser").html() + "</span></a> </b> </p>  <p><div class='alert-message block-message info'>" + $('#message').val() + "</div> </p>");
         return false;
     });
 });
